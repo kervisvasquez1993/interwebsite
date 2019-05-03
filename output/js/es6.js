ab9ca3c1fbee3f7124cadf083ddc85a5c6d3ebfb
public <public@america-12345>
2019-05-03 10:12:32 -0400

    archivos agregado

--- output/js/es6.js
+++ output/js/es6.js
@@ -3,6 +3,10 @@
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems,{
-    	interval: 3000
+      interval: 3000,
+      height:400,
+      indicators : false
     });
+    var elems2 =document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems2);
   });
