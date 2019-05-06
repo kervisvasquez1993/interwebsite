0c84071abe4079ad8eb0044ed7b8bf8b51a4ab0a
public <public@america-12345>
2019-05-06 08:20:28 -0400

    kervis3

--- output/js/es6.js
+++ output/js/es6.js
@@ -4,9 +4,18 @@
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems,{
       interval: 3000,
-      height:400,
       indicators : false
     });
     var elems2 =document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems2);
+
+    var mb =document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(mb);
   });
+var btn_pintar = document.getElementById('btn_pintar')
+var kervis_pintar = document.getElementById('kervis-pintar');
+btn_pintar.addEventListener('click',kervis);
+
+function kervis() {
+ kervis_pintar.style.display='block';
+}
